06ea7b6eced3378ea6a31b502a5e3f36cdc89ae5
Fenixfo <jair-07020@hotmail.com>
2021-12-26 18:16:15 -0500

    CRUD creation of the ITEM model

--- METGroup/api/urls.py
+++ METGroup/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls.conf import include
-from .views import ItemView, ItemsList, StoreList, StoreView, StoreViewAll
+from .views import ItemsList, StoreList, StoreView, StoreViewAll, ItemViewAll
+# from .views import ItemView, ItemsList, StoreList, StoreView, StoreViewAll
 from . import views
 
 urlpatterns = [
@@ -8,10 +9,9 @@ urlpatterns = [
     path('itemsOriginal/', ItemsList.as_view(), name='items_list'),
     path('auth', views.auth),
     path('register', views.register),
-    # path('api/', include('api.urls'))
-    # path('store/<str:name>', views.newStore),
+    path('item/<str:name>', views.newItem),
     path('store/<str:name>', StoreView.as_view(), name='store_process'),
-    path('item/<str:name>', ItemView.as_view(), name='item_process'),
+    # path('item/<str:name>', ItemView.as_view(), name='item_process'),
     path('stores/', StoreViewAll.as_view(), name='stores_process'),
-    # path('item/', ItemViewAll.as_view(), name='item_process'),
+    path('items/', ItemViewAll.as_view(), name='item_process'),
 ]
--- METGroup/api/views.py
+++ METGroup/api/views.py
@@ -8,7 +8,7 @@ from rest_framework import generics, serializers
 from .models import Items, Store
 from .serializers import ItemsSerializer, StoreSerializer
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
@@ -16,14 +16,18 @@ from django.contrib.auth.hashers import check_password
 from django.http import JsonResponse
 # Create your views here.
 
+#Obtener todos los datos de Store
 class StoreList(generics.ListCreateAPIView):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
+#Obtener todos los datos de List
 class ItemsList(generics.ListCreateAPIView):
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
 
+#Autenticación de un Usuario
+actualUser=''
 @api_view(['POST'])
 def auth(request):
     try:
@@ -51,8 +55,10 @@ def auth(request):
     data = {
         'access_token':token.key
     }
+    actualUser=username
     return JsonResponse(data)
 
+#Registro de un Usuario
 @api_view(['POST'])
 def register(request):
     try:
@@ -82,13 +88,13 @@ def register(request):
     }
     return JsonResponse(data)
 
-
+#Store CRUD
 class StoreView(View):
-
+    #Políticas de CORS
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-
+    #Store GET
     def get(self, request, name):
         if name == '':
             return JsonResponse({'message':'Invalid name store'})
@@ -104,7 +110,7 @@ class StoreView(View):
                 return JsonResponse(data)
             except:
                 return JsonResponse({'message':'Store name does not exist'})
-
+    #Store POST
     def post(self, request, name):
         if name == '':
             return JsonResponse({'message':'Invalid name store'})
@@ -121,14 +127,7 @@ class StoreView(View):
             'items':[]
         }
         return JsonResponse(data)
-
-    # def put(self, request, name):
-    #     jsonData = json.loads(request.body)
-    #     stores = list(Store.objects.filter(name=name).values())
-    #     if len(stores) > 0:
-    #         store = Store.objects.get(name=name)
-    #         store.
-
+    #Store DELETE
     def delete(self, request, name):
         store = list(Store.objects.filter(name=name).values())
         if len(store) > 0:
@@ -136,82 +135,122 @@ class StoreView(View):
             return JsonResponse({'message':'Store deleted'})
         else:
             return JsonResponse({'message':'Store name does not exist'})
-        
 
+# Store GET(all)
 class StoreViewAll(View):
-
+    #Políticas de CORS
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-
+    #Store GET(all)
     def get(self, request):
         stores = Store.objects.values()
         for store in stores:
             store['items'] = list(Items.objects.filter(storeId=store['id']).values())
-            print(store)
-        print(list(stores))
         return JsonResponse({'stores':list(stores)})
 
 
-class ItemView(View):
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
-    def get(self, request, name):
+#Item CRUD
+@api_view(['GET','POST','PUT','DELETE'])
+def newItem(request,name):
+    #Item POST
+    if request.method == 'POST':
+        try:
+            price = request.data['price']
+            storeId_id = request.data['storeId']
+        except:
+            data = {
+                'price':'This field is required.',
+                'storeId':'This field is required.'
+            }
+            return JsonResponse(data)
         if name == '':
-            return JsonResponse({'message':'Invalid name store'})
+            return JsonResponse({'message':'Invalid name item'})
         else:
             try:
-                store = Store.objects.get(name=name)
-                items = Items.objects.filter(storeId=store.id).values()
+                try:
+                    store = Store.objects.get(id=storeId_id)
+                except:
+                    return JsonResponse({'message':'Store id does not exist'})
+                item = Items.objects.get(name=name)#, storeId_id=storeId_id)
+                return JsonResponse({'message':'Name item already exists'})
+            except:
+                Items.objects.create(name=name, price=price, storeId_id=storeId_id)
+        item = Items.objects.get(name=name, storeId_id=storeId_id)
+        data = {
+            'id':item.id,
+            'name':item.name,
+            'price':item.price,
+            'storeId':item.storeId_id
+        }
+        return JsonResponse(data)
+    #Item GET
+    if request.method == 'GET':
+        if name == '':
+            return JsonResponse({'message':'Invalid name Item'})
+        else:
+
+            try:
+                item = Items.objects.filter(name=name).values()[0]
                 data = {
-                        'id':store.id,
-                        'name':store.name,
-                        'items':list(items)
+                        'id':item['id'],
+                        'name':item['name'],
+                        'price':item['price'],
+                        'store_id':item['storeId_id']
                     }
                 return JsonResponse(data)
             except:
-                return JsonResponse({'message':'Store name does not exist'})
+                return JsonResponse({'message':'Item name does not exist'})
+    #Item PUT
+    if request.method == 'PUT':
 
+        try:
+            price = request.data['price']
+            storeId_id = request.data['storeId']
+        except:
+            data = {
+                'price':'This field is required.',
+                'storeId':'This field is required.'
+            }
+            return JsonResponse(data)
+        if price == '':
+            return JsonResponse({'message':'Invalid price item'})
+        else:
+            try:
+                store = Store.objects.get(id=storeId_id)
+            except:
+                return JsonResponse({'message':'Store id does not exist'})
+        try:
+            item = Items.objects.get(name=name)
+            item.price = price
+            item.storeId_id = storeId_id
+            item.save()
+            data = {
+                'id':item.id,
+                'name':item.name,
+                'price':item.price,
+                'storeId':item.storeId_id
+            }
+        except:
+            data = {'message':'Item not found'}
+        return JsonResponse(data)
+    #Item DELETE
+    if request.method == 'DELETE':
+        item = list(Items.objects.filter(name=name).values())
+        if len(item) > 0:
+            Items.objects.filter(name=name).delete()
+            return JsonResponse({'message':'Item deleted'})
+        else:
+            return JsonResponse({'message':'Item name does not exist'})
 
-# A continuación se muestra una forma diferente de obtener y enviar datos
+# Store GET(all)
+class ItemViewAll(View):
+    #Políticas de CORS
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    #Item GET(all)
+    def get(self, request):
+        items = Items.objects.values()
+        return JsonResponse({'items':list(items)})
 
-# @api_view(['POST'])
-# def newStore(request, name):
-# def post(self, request, name):
-#     if name == '':
-#         return Response('Invalid name store')
-#     else:
-#         try:
-#             store = Store.objects.get(name=name)
-#             return Response('Name store already exists')
-#         except:
-#             Store.objects.create(name=name)
-#     store = Store.objects.get(name=name)
-#     data = {
-#         'id':store.id,
-#         'name':store.name,
-#         'items':[]
-#     }
-#     return JsonResponse(data)
-
-
-# @api_view(['GET'])
-# def newStore(request, name):
-# def get(self, request, name):
-#     if name == '':
-#         return Response('Invalid name store')
-#     else:
-#         try:
-#             store = Store.objects.get(name=name)
-#             items = Items.objects.filter(storeId=store.id).values()
-#             data = {
-#                     'id':store.id,
-#                     'name':store.name,
-#                     'items':list(items)
-#                 }
-#             return JsonResponse(data)
-#         except:
-#             return Response('Store name does not exist')
